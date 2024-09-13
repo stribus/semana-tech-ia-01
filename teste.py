b2571f78335226a59597026b735557ca0f9eaff4
stribus <paladinotoldo@gmail.com>
2024-09-13 20:51:19 -0300

    alguns ajustes para adequar a minha visão de sistema

--- teste.py
+++ teste.py
@@ -9,13 +9,17 @@ from crewai import Agent, Task, Crew, Process
 from langchain.tools import Tool
 from langchain_openai import ChatOpenAI
 from langchain_community.tools import DuckDuckGoSearchResults
+from dotenv import load_dotenv
 
-import streamlit as st
+load_dotenv()
+
+#import streamlit as st
 
 
 # IMPORTANDO OPENAI LLM - GPT
-os.environ['OPENAI_API_KEY']=st.secrets['OPEN_API_KEY']
+os.environ['OPENAI_API_KEY']=os.getenv('OPEN_API_KEY')
 llm = ChatOpenAI(model="gpt-3.5-turbo")
+
 
 # CRIANDO YAHOO FINANCE TOOL 
 def fetch_stock_price(ticket):
@@ -35,7 +39,7 @@ search_tool = DuckDuckGoSearchResults(backend='news', num_results=10)
 
 stockPriceAnalyst = Agent(
     role="Analista de Ações",
-    goal="Encontrar o preço da ação {ticket} e analisar as tendências em português.",
+    goal="Encontrar o preço da ação {ticket} e analisar as tendências.",
     backstory="""Você é altamente experiente em analisar o preço de uma ação específica e fazer previsões sobre seu preço futuro.""",
     verbose=True,
     llm= llm,
@@ -60,7 +64,7 @@ getStockPrice = Task(
 newsAnalyst = Agent(
     role="Analista de Notícias de Ações",
     goal="""Criar um resumo curto das notícias de mercado relacionadas à empresa de ações {ticket}. Especifique a tendência atual - alta, baixa ou lateral com
-    o contexto das notícias. Para cada ativo de ações solicitado, especifique um número entre 0 e 100, onde 0 é medo extremo e 100 é ganância extrema. em português.""",
+    o contexto das notícias. Para cada ativo de ações solicitado, especifique um número entre 0 e 100, onde 0 é medo extremo e 100 é ganância extrema.""",
     backstory="""Você é altamente experiente em analisar as tendências e notícias do mercado e acompanha ativos há mais de 10 anos.
 
     Você também é um analista de nível mestre nos mercados tradicionais e tem um profundo entendimento da psicologia humana.
@@ -100,7 +104,7 @@ get_news = Task(
 
 stockAnalystWrite = Agent(
     role = "Analista de Ações senior",
-    goal= """Use a tendência de preço da ação e o relatório de notícias da ação para criar uma análise e escrever o boletim informativo sobre a empresa {ticket} que seja breve e destaque os pontos mais importantes. Foque na tendência de preço da ação, notícias e pontuação de medo/ganância. Quais são as considerações para o futuro próximo? Inclua as análises anteriores da tendência de ações e resumo de notícias. respostas em português.""",
+    goal= """Use a tendência de preço da ação e o relatório de notícias da ação para criar uma análise e escrever o boletim informativo sobre a empresa {ticket} que seja breve e destaque os pontos mais importantes. Foque na tendência de preço da ação, notícias e pontuação de medo/ganância. Quais são as considerações para o futuro próximo? Inclua as análises anteriores da tendência de ações e resumo de notícias.""",
     backstory= """Você é amplamente aceito como o melhor analista de ações do mercado. Você entende conceitos complexos e cria histórias e narrativas convincentes que ressoam com um público mais amplo. Você entende fatores macro e combina várias teorias - por exemplo, teoria do ciclo e análises fundamentais. Você é capaz de manter várias opiniões ao analisar qualquer coisa.""",
     verbose = True,
     llm=llm,
@@ -141,19 +145,6 @@ crew = Crew(
     max_iter=15
 )
 
+results= crew.kickoff(inputs={'ticket': 'ITUB3.SA'})
 
-
-with st.sidebar:    
-    st.header('Digite a ação para pesquisar')
-
-    with st.form(key='research_form'):
-        topic = st.text_input("Ação", value="AAPL", help="Digite a ação para pesquisar")
-        submit_button = st.form_submit_button(label = "rodar pesquisa")
-if submit_button:
-    if not topic:
-        st.error("Por favor, insira uma ação para pesquisar")
-    else:
-        results= crew.kickoff(inputs={'ticket': topic})
-
-        st.subheader("Resultado da pesquisa")
-        st.write(results['final_output'])
+print(results)
